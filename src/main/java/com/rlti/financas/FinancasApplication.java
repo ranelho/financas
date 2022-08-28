8d8be8591c4eb4c09e621ac8806ed3bb23fd23cf
Ranelho Lacerda <ranelhol@gmail.com>
2022-08-28 08:40:38 -0300

    implemente fluxo log despesa controller

--- src/main/java/com/rlti/financas/FinancasApplication.java
+++ src/main/java/com/rlti/financas/FinancasApplication.java
@@ -3,8 +3,12 @@ package com.rlti.financas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class FinancasApplication {
 
 	@GetMapping
--- src/main/java/com/rlti/financas/despesas/domain/Despesa.java
+++ src/main/java/com/rlti/financas/despesas/domain/Despesa.java
@@ -1,9 +1,7 @@
 package com.rlti.financas.despesas.domain;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
