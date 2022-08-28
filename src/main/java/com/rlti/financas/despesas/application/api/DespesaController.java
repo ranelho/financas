1f33da8baa19d221f6587fec05c09396b1ce27b7
Ranelho Lacerda <ranelhol@gmail.com>
2022-08-28 09:14:03 -0300

    implementa controller e fluxo para service

--- src/main/java/com/rlti/financas/despesas/application/api/DespesaController.java
+++ src/main/java/com/rlti/financas/despesas/application/api/DespesaController.java
@@ -4,18 +4,22 @@ import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class DespesaController implements DespesaApi {
+	
+	private final DespesaService despesaService;
 
 	@Override
 	public DespesaResponse postDespesa(@Valid DespesaRequest despesaRequest) {
 		log.info("[inicia] DespesaController - postDespesa");
-		log.info("[finaliza] DespesaController - postDespesa");
-		
-		return null;
+		DespesaResponse despesaCriada = despesaService.criaDespesa(despesaRequest);
+		log.info("[finaliza] DespesaController - postDespesa");		
+		return despesaCriada;
 	}
 
 }
---
+++ src/main/java/com/rlti/financas/despesas/application/api/DespesaService.java
@@ -0,0 +1,5 @@
+package com.rlti.financas.despesas.application.api;
+
+public interface DespesaService {
+	DespesaResponse criaDespesa(DespesaRequest despesaRequest);
+}
